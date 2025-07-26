696f06fa4d34d0464fbc9348b6eb5425a36c948e
rajkumar-bhange <rajkumarbh45@gmail.com>
2025-07-26 18:56:37 +0530

    added new file

---
+++ tests/api.test.js
@@ -0,0 +1,9 @@
+const { test, expect, request } = require('@playwright/test');
+
+test('Check Dummy API Status', async () => {
+  const apiContext = await request.newContext();
+  const response = await apiContext.get('https://jsonplaceholder.typicode.com/posts/1');
+  expect(response.status()).toBe(200);
+  const body = await response.json();
+  expect(body).toHaveProperty('id', 1);
+});
